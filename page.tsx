6168492cf346e3468bd25ac4d425d867756d1d48
Ahmed Memon <160051377+AhmedMemon1234@users.noreply.github.com>
2024-09-29 20:13:15 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header"
+
+export default function Aboutus(){
+    return(
+        <div>
+                        <img className="image" src="https://images.pexels.com/photos/531880/pexels-photo-531880.jpeg?cs=srgb&dl=pexels-pixabay-531880.jpg&fm=jpg" alt="" ></img>
+
+            <Header/>
+      <h1 className="Text">Hey Welcome To About Us Section</h1>
+        </div>
+    )
+}
